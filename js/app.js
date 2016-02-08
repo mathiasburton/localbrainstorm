3681105eb129ef5fa0034a89baa3e6530ff07aff
Mathias Burton <bonjour@mathiasburton.com>
2016-02-07 23:37:39 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+var myApp = angular.module('myApp',
+  ['ngRoute', 'firebase'])
+  .constant('FIREBASE_URL', 'https://localbrainstorm.firebaseio.com/');
+
+
+myApp.run(['$rootScope', '$location',
+  function($rootScope, $location) {
+    $rootScope.$on('$routeChangeError',
+      function(event, next, previous, error) {
+        if (error=='AUTH_REQUIRED') {
+          $rootScope.message = 'Sorry, you must log in to access that page';
+          $location.path('/login');
+        } // AUTH REQUIRED
+      }); //event info
+  }]); //run
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'RegistrationController'
+    }).
+    when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'RegistrationController'
+    }).
+    when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'RegistrationController'
+    }).
+    when('/success', {
+      templateUrl: 'views/success.html',
+      controller: 'SuccessController',
+      resolve: {
+        currentAuth: function(Authentication) {
+          return Authentication.requireAuth();
+        } //current Auth
+      } //resolve
+    }).
+    otherwise({
+      redirectTo: '/login'
+    });
+}]);
