a277f3981855d46a3783c1c7897a200f097f9564
Rob Middleton <rob@middlerob.com>
2016-05-25 19:33:13 -0700

    remove .tmp from git

--- .tmp/public/js/controllers/mainController.js
+++
@@ -1,9 +0,0 @@
-brainstormModule.controller("mainController", function($scope) {
-
-	var ctrl = this;
-
-	this.scrollTo = function (anchor) {
-		$("html, body").animate({ scrollTop:
-			$(anchor).offset().top }, 1000);
-	};
-})
